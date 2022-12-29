8315f92dbde09d93049cadd8ab7a4edb8f0f0f9b
Cople <cople7@gmail.com>
2022-12-29 11:22:51 +0800

    image placeholder

--- app.js
+++ app.js
@@ -1,59 +1,28 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 const port = process.env.PORT || 3001;
 
-app.get("/", (req, res) => res.type('html').send(html));
+app.get('/:size/:background?/:textColor?', (req, res, next) => {
+  let [width, height] = req.params.size.split('x');
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+  if (!height) height = width;
 
+  if (isNaN(width) || isNaN(height)) return next();
 
-const html = `
-<!DOCTYPE html>
-<html>
-  <head>
-    <title>Hello from Render!</title>
-    <script src="https://cdn.jsdelivr.net/npm/canvas-confetti@1.5.1/dist/confetti.browser.min.js"></script>
-    <script>
-      setTimeout(() => {
-        confetti({
-          particleCount: 100,
-          spread: 70,
-          origin: { y: 0.6 },
-          disableForReducedMotion: true
-        });
-      }, 500);
-    </script>
-    <style>
-      @import url("https://p.typekit.net/p.css?s=1&k=vnd5zic&ht=tk&f=39475.39476.39477.39478.39479.39480.39481.39482&a=18673890&app=typekit&e=css");
-      @font-face {
-        font-family: "neo-sans";
-        src: url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/l?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff2"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/d?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/a?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("opentype");
-        font-style: normal;
-        font-weight: 700;
-      }
-      html {
-        font-family: neo-sans;
-        font-weight: 700;
-        font-size: calc(62rem / 16);
-      }
-      body {
-        background: white;
-      }
-      section {
-        border-radius: 1em;
-        padding: 1em;
-        position: absolute;
-        top: 50%;
-        left: 50%;
-        margin-right: -50%;
-        transform: translate(-50%, -50%);
-      }
-    </style>
-  </head>
-  <body>
-    <section>
-      Hello from Render!
-    </section>
-  </body>
-</html>
-`
+  const background = req.params.background || 'DDD';
+  const textColor = req.params.textColor || '777';
+  const fontSize = req.query.fontSize || 24;
+  const text = req.query.text || `${width}×${height}`;
+
+  console.log(req.originalUrl);
+
+  res
+    .set('Cache-Control', `public, max-age=${365 * 24 * 60 * 60}`)
+    .type('image/svg+xml')
+    .send(`<svg xmlns="http://www.w3.org/2000/svg" width="${width}" height="${height}" viewBox="0 0 ${width} ${height}">
+      <rect fill="#${background}" width="${width}" height="${height}"/>
+      <text fill="#${textColor}" font-family="monospace, sans-serif" font-size="${fontSize}" text-anchor="middle" dominant-baseline="middle" x="50%" y="50%">${text}</text>
+    </svg>`)
+});
+
+app.listen(port, () => console.log(`app listening on port ${port}!`));
